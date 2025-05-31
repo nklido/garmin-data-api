66e602f752b23f0b1d3b33a86607549dd76b9384
nklido <nikos.klido@gmail.com>
2025-05-31 16:53:29 +0300

    Activity mapper add null handle null values for names, speed & heart rate

--- src/main/java/com/nklido/garmin/data/api/mapper/ActivityMapper.java
+++ src/main/java/com/nklido/garmin/data/api/mapper/ActivityMapper.java
@@ -6,20 +6,23 @@ import com.nklido.garmin.data.api.core.model.EffortLabel;
 import com.nklido.garmin.data.api.util.PaceConverter;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class ActivityMapper {
     public static Activity toDomain(ActivityDto activityDto) {
         Activity activity = new Activity();
 
         activity.setId(activityDto.getActivityId());
-        activity.setName(activityDto.getActivityName());
+        activity.setName(Optional.ofNullable(activityDto.getActivityName())
+          .orElse(""));
         activity.setStartTimeGMT(activityDto.getStartTimeGMT());
         activity.setStartTimeLocal(activityDto.getStartTimeLocal());
         activity.setDuration(activityDto.getDuration());
         activity.setDistance(activityDto.getDistance());
-        activity.setAverageSpeed(activityDto.getAverageSpeed());
+        activity.setAverageSpeed(Optional.ofNullable(activityDto.getAverageSpeed()).orElse(0.0));
         activity.setActivityType(ActivityTypeMapper.toDomain(activityDto.getActivityType()));
-        activity.setAverageHR(activityDto.getAverageHR());
+        activity.setAverageHR(Optional.ofNullable(activityDto.getAverageHR()).orElse(0.0));
 
         activity.setHrTimeInZone1(activityDto.getHrTimeInZone1());
         activity.setHrTimeInZone2(activityDto.getHrTimeInZone2());
